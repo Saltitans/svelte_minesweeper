e81ae92d745bcf25235fb9bf257b4011389568d5
Tomás Figueiredo <tomasviana_cof@hotmail.com>
2024-12-09 14:58:42 +0000

    Init project + build board

---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import '../app.css';
+	import Board from './components/Board.svelte';
+</script>
+
+<div class="flex h-screen w-screen flex-row justify-center">
+	<Board />
+</div>
---
+++ src/routes/components/Cell.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import '../../app.css';
+
+	let { content } = $props();
+</script>
+
+<div class="flex h-10 w-10 items-center justify-center border bg-yellow-500">
+	{content}
+</div>
